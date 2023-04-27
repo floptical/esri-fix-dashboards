9739aa44036b871c5ad0f75d9a0a9677ab07e2ff
floptical <stephen.macdavid@gmail.com>
2023-04-27 17:07:23 -0400

    major enhancements, find field names based on found datasource ids

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ import urllib.parse
 
 
 def main(itemid, new_datasource_itemid=None):
-    '''Takes a dashboard of {itemid}, gets it's JSON, finds field names and makes them lower-case,
+    """Takes a dashboard of {itemid}, gets it's JSON, finds field names and makes them lower-case,
     then posts it back to AGO.
     Optionally takes a new itemid for a new datasource, and updates it in-line as well. Likely of limited use
-    since dashboards can have multiple datasources.'''
+    since dashboards can have multiple datasources."""
+
     token_url = 'https://arcgis.com/sharing/rest/generateToken'
     data = {'username': 'ago-user',
             'password': 'password',
             'referer': 'https://www.arcgis.com',
             'f': 'json'}
     ago_token = requests.post(token_url, data).json()['token']
+
+    # Our unique AGO organization id
+    org_id = 'some_org_id'
 
     # Request the "data" json which contains field names and actual meat of the dashboard
     data_url = f'https://www.arcgis.com/sharing/rest/content/items/{itemid}/data'
@@ -32,35 +36,34 @@ def main(itemid, new_datasource_itemid=None):
     # Save all found field names in a set so we can later come back and modify "expression" and "text" elements
     # intelligently without having to worry about complex regex or string manipulation.
     found_field_names = set()
+    found_datasource_itemids = set()
 
     # Recursive function to run through and modify field names as we go
     def find_and_modify_field_names(json_obj):
         # If the value we're on is a dictionary, check keys and potentially recurse on the value.
         if isinstance(json_obj, dict):
             for key, value in json_obj.items():
-                # If we were passed a new item id..
-                if new_datasource_itemid:
-                    if key == "itemId":
-                        json_obj[key] = new_datasource_itemid
-                # Safety check for specific calls to layers within a dataSource that aren't simply 0
-                # If it's not using the first layer, which is 0, then fail out.
-                # We should not modify programmatically in this case.
-                if key == 'layerId' and new_datasource_itemid:
-                    if str(value) != '0':
+                if key == "dataSource":
+                    if json_obj["dataSource"]["type"] == 'itemDataSource':
+                        found_datasource_itemids.add(json_obj["dataSource"]["itemId"])
+                    if json_obj["dataSource"]["layerId"] == 0 and new_datasource_itemid:
+                        json_obj["dataSource"]["itemId"] = new_datasource_itemid
+                    elif json_obj["dataSource"]["layerId"] != 0 and found_datasource_itemids:
+                        # Safety check for specific calls to layers within a dataSource that aren't simply 0
+                        # If it's not using the first layer, which is 0, then fail out.
+                        # We should not modify programmatically in this case.
                         raise Exception(f'''
-                            Found a specific layerId reference: {str(value)}!'
-                            Cannot update with new datasource itemId programmatically! Please make your itemId
-                            changes manually through assistant.esri-ps.com.
-                            ''')
+                                Found a specific layerId reference: {str(value)}!'
+                                Cannot update with new datasource itemId programmatically! Please make your itemId
+                                changes manually through assistant.esri-ps.com.
+                                ''')
 
                 # if we have a field key, then the sub value is also a dict containing info about that field.
                 if key == "field":
-                    for sub_key, sub_value in value.items():
-                        # find the 'name' element which has the field name as a value.
-                        if sub_key == "name":
-                            if sub_value:
-                                found_field_names.add(sub_value.upper())
-                                json_obj[key][sub_key] = sub_value.lower()
+                    if json_obj["field"]["name"]:
+                        field_name = json_obj["field"]["name"]
+                        found_field_names.add(field_name.upper())
+                        json_obj["field"]["name"] = field_name.lower()
 
                 # This has a direct field name value, lower-case if not empty
                 if key == "fieldName":
@@ -154,12 +157,15 @@ def main(itemid, new_datasource_itemid=None):
 
                 if key == "text":
                     # Use found_field_names set to replace field name strings with the lower_case version.
+                    # NOTE: need to grab field names from source layers, we won't always find field names elsewhere.
                     if value:
                         for i in found_field_names:
                             # Using regex, find all 'i' strings, and then substitute out a lower-case version
                             # ignore case otherwise several similar field names will result in inconsistent case.
-                            pattern = re.compile(i, re.IGNORECASE)
-                            value = pattern.sub(i.lower(), value)
+                            # Also all we care about in "text" fields are field names surrounded by curly braces,
+                            # so only search and replace for that
+                            pattern = re.compile('{' + i + '}', re.IGNORECASE)
+                            value = pattern.sub('{' + i.lower() + '}', value)
                         json_obj[key] = value
 
                 if key == 'expression':
@@ -180,10 +186,37 @@ def main(itemid, new_datasource_itemid=None):
             for item in json_obj:
                 find_and_modify_field_names(item)
 
+    def gather_datasource_field_names():
+        """Using our found datasource itemids, collect all field names for the datasource. Ths is important because
+        "text" and "expression" values can contain field names not used anywhere else where we would have recorded them
+         in our first run of find_and_modify_field_names()."""
+        for i in found_datasource_itemids:
+            search_url = 'https://www.arcgis.com/sharing/rest/search'
+            params = {'f': 'json',
+                      'q': i,
+                      'token': ago_token
+                      }
+            response = requests.get(search_url, params=params)
 
+            #print(json.dumps(response.json(), indent=2))
+            if response.json()["results"][0]["type"] == 'Table' or response.json()["results"][0]["type"] == 'Feature Layer' or response.json()["results"][0]["type"] == 'Feature Service':
+                dataset_name = response.json()["results"][0]["name"]
+                # We can query for datasource json and gdet field names that way:
+                datasource_url = f'https://services.arcgis.com/{org_id}/ArcGIS/rest/services/{dataset_name}/FeatureServer/0?f=pjson&token={ago_token}'
+                response2 = requests.get(datasource_url)
+                #print(json.dumps(response2.json(), indent=2))
+                for i in response2.json()['fields']:
+                    field_name = i['name']
+                    # Add to our found_field_names set for use in our 2nd run of find_and_modify_field_names()
+                    found_field_names.add(field_name.upper())
+
+    # first run where we get datasource itemids and field names used in the dashboard json.
     find_and_modify_field_names(parsed_json)
 
-    print(set(found_field_names))
+    print(f'found_datasource_itemids: {found_datasource_itemids}')
+    gather_datasource_field_names()
+    print(f'found_field_names: {found_field_names}')
+
     # Run a second time after our found_field_names set is fully populated
     find_and_modify_field_names(parsed_json)
 
@@ -220,8 +253,6 @@ def main(itemid, new_datasource_itemid=None):
 
 if __name__ == '__main__':
     itemid = 'some_dashboard_itemid'
-    optional_new_datasource_itemid = 'optional_new_datasource_itemid'
-
-    # Test next: CampFin_desktop_1_1 a904a53d21d24f2da1dcd27fc2caa16a
+    #optional_new_datasource_itemid = 'optional_new_datasource_itemid'
 
     main(itemid)
