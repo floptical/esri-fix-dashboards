72eeb44e32ca2966a1406fbaf7d125ab62b5303e
floptical <stephen.macdavid@gmail.com>
2023-04-28 11:41:00 -0400

    Additional enhancements, Click args, readme, gitignore

---
+++ README.md
@@ -0,0 +1,30 @@
+# esri-fix-dashboards
+
+This script is intended to be used for Oracle to PostgreSQL datasource migrations. When you overwrite a datasource
+originally published from Oracle, all the field names will change from upper-case to lower-case. This breaks
+AGO dashboards in strange ways, but if you modify the field names to lower-case, it un-breaks them.
+
+To use this script, point it at an AGO dashboard's itemid to run through it and change all found field names from
+upper-case to lower-case. Also know all data sources used first and put them in the var "expected_datasource_ids".
+
+Example usage:
+
+```
+python .\main.py \
+        --ago-user some_user \
+        --ago-password password \
+        --org-id your_org_id \
+        --target_dashboard_itemid 123abc \
+        --expected-datasource-itemids 456abc,789dfg
+        --dry-run
+        
+# Double-check differences of your dashboard, names of the saved json will use the itemid:
+diff 123abc-ORIGINAL-dashboard.json 123abc-MODIFIED-dashboard.json 
+
+python .\main.py \
+        --ago-user some_user \
+        --ago-password password \
+        --org-id your_org_id \
+        --target_dashboard_itemid 123abc \
+        --expected-datasource-itemids 456abc,789dfg
+```
--- main.py
+++ main.py
@@ -3,39 +3,63 @@ import requests
 import json
 import re
 import urllib.parse
+import click
 
 
-def main(itemid, expected_datasource_ids, new_datasource_itemid=None):
+@click.command()
+@click.option('--ago-user', required=True)
+@click.option('--ago-password', required=True)
+@click.option('--org-id', required=True)
+@click.option('--target_dashboard_itemid', required=True)
+@click.option('--expected-datasource-itemids', default=None, required=True,
+              help='Pass one or multiple datasource ids, comma-separated')
+@click.option('--new-datasource-itemid', default=None, required=False,
+              help='New itemid for a dashboards datasources. \
+                   Only supported if there is 1 datasource used in the dashboard')
+@click.option('--dry-run', default=None, is_flag=True, required=False,
+              help='Dry run. Dont update with modified code, just write to local files.')
+def main(ago_user, ago_password, org_id, target_dashboard_itemid, expected_datasource_itemids, new_datasource_itemid, dry_run):
     """Takes a dashboard of {itemid}, gets it's JSON, finds field names and makes them lower-case,
     then posts it back to AGO.
     Optionally takes a new itemid for a new datasource, and updates it in-line as well. Likely of limited use
     since dashboards can have multiple datasources."""
 
-    if new_datasource_itemid and len(expected_datasource_ids) > 1:
+    expected_datasource_itemids = expected_datasource_itemids.split(',')
+
+    if not expected_datasource_itemids:
+        raise AssertionError('No "expected_datasource_ids" var passed! You should know what data sources a dashboard \
+        is using before attempting a large modification like this, Because if you dont update all the data sources to\
+        have lower-case field names, it will break.')
+
+    if new_datasource_itemid and len(expected_datasource_itemids) > 1:
         print('Not changing out datasource itemids since we expect there to be more than 1 in the dashboard.')
         new_datasource_itemid = None
 
     token_url = 'https://arcgis.com/sharing/rest/generateToken'
-    data = {'username': 'ago-user',
-            'password': 'password',
+    data = {'username': ago_user,
+            'password': ago_password,
             'referer': 'https://www.arcgis.com',
             'f': 'json'}
     ago_token = requests.post(token_url, data).json()['token']
 
-    # Our unique AGO organization id
-    org_id = 'some_org_id'
-
     # Request the "data" json which contains field names and actual meat of the dashboard
-    data_url = f'https://www.arcgis.com/sharing/rest/content/items/{itemid}/data'
+    data_url = f'https://www.arcgis.com/sharing/rest/content/items/{target_dashboard_itemid}/data'
     params = {
         'f': 'json',
         'token': ago_token
     }
+    print(data_url)
     response = requests.get(data_url, params=params)
     #print(response.text)
     assert response.status_code == 200
     parsed_json = json.loads(response.text)
     #print(json.dumps(parsed_json, indent=2))
+
+    # Write locally before modification
+    f = open(f'./{target_dashboard_itemid}-ORIGINAL-dashboard.json', "w")
+    # Write with indent so it's readable while I'm spot-checking
+    f.write(json.dumps(parsed_json, indent=2))
+    f.close()
 
     # Save all found field names in a set so we can later come back and modify "expression" and "text" elements
     # intelligently without having to worry about complex regex or string manipulation.
@@ -53,7 +77,7 @@ def main(itemid, expected_datasource_ids, new_datasource_itemid=None):
                             found_datasource_itemids.add(json_obj["dataSource"]["itemId"])
                         if json_obj["dataSource"]["layerId"] == 0 and new_datasource_itemid:
                             json_obj["dataSource"]["itemId"] = new_datasource_itemid
-                        elif json_obj["dataSource"]["layerId"] != 0 and found_datasource_itemids:
+                        elif json_obj["dataSource"]["layerId"] != 0 and new_datasource_itemid:
                             # Safety check for specific calls to layers within a dataSource that aren't simply 0
                             # If it's not using the first layer, which is 0, then fail out.
                             # We should not modify programmatically in this case.
@@ -63,10 +87,11 @@ def main(itemid, expected_datasource_ids, new_datasource_itemid=None):
                                     changes manually through assistant.esri-ps.com.
                                     ''')
 
-                # Reference this dashboard to figure this out: 32fc0cb303f547ce89a906a6c743b13b
+                # Seems to be web-map specific but could be used in dashboards?
                 if key == 'operationalLayers':
-                    if json_obj['operationalLayers']['layerType'] == 'ArcGISFeatureLayer':
-                        found_datasource_itemids.add(json_obj['layerType']["itemId"])
+                    for i in json_obj["operationalLayers"]:
+                        if i["layerType"] == 'ArcGISFeatureLayer':
+                            found_datasource_itemids.add(i["itemId"])
 
                 # if we have a field key, then the sub value is also a dict containing info about that field.
                 # Edit: in some dashboards, it can apparently be a direct value
@@ -91,7 +116,12 @@ def main(itemid, expected_datasource_ids, new_datasource_itemid=None):
                 if key == "onStatisticField":
                     if value:
                         found_field_names.add(value.upper())
-                        json_obj[key] = value.lower()
+                        json_obj["onStatisticField"] = value.lower()
+                # Also lower-case the new made statistic field name
+                if key == "outStatisticFieldName":
+                    if value:
+                        # Don't add these to found_field_names because they're not a datasource field name
+                        json_obj["outStatisticFieldName"] = value.lower()
 
                 # This has a direct field name value, lower-case if not empty
                 if key == "valueField":
@@ -194,8 +224,7 @@ def main(itemid, expected_datasource_ids, new_datasource_itemid=None):
                             value = pattern.sub(i.lower(), value)
                         json_obj[key] = value
 
-                # example: cdb54ca79c94440aba9eb63a7999b206
-                # TODO: THIS NEEDS TESTING
+                # Seems to be web-map specific but could be used in dashboards?
                 if key == 'labelExpressionInfo':
                     sub_value = json_obj['labelExpressionInfo']['value']
                     # Using regex, find all 'i' strings, and then substitute out a lower-case version
@@ -218,21 +247,29 @@ def main(itemid, expected_datasource_ids, new_datasource_itemid=None):
         """Using our found datasource itemids, collect all field names for the datasource. Ths is important because
         "text" and "expression" values can contain field names not used anywhere else where we would have recorded them
          in our first run of find_and_modify_field_names()."""
+
         for i in found_datasource_itemids:
             search_url = 'https://www.arcgis.com/sharing/rest/search'
             params = {'f': 'json',
                       'q': i,
                       'token': ago_token
                       }
+            print(f'\nSearching our datasource IDs with {search_url}')
             response = requests.get(search_url, params=params)
 
-            #print(json.dumps(response.json(), indent=2))
             if response.json()["results"][0]["type"] == 'Table' or response.json()["results"][0]["type"] == 'Feature Layer' or response.json()["results"][0]["type"] == 'Feature Service':
-                # Loop through all layers
+                # Loop through all layers we found, somehow esri is returning back multiple matches even when given a direct itemid
                 for layer in response.json()["results"]:
-                    dataset_name = layer["name"]
+                    #print(json.dumps(response.json(), indent=2))
+                    # if we got multiple hits somehow (why esri), then confirm itemid matches
+                    if layer["id"] == i:
+                        dataset_name = layer["name"]
+                    else:
+                        print(f'Ignoring bad match {layer["url"]}')
+                        continue
                     # We can query for datasource json and get field names that way:
                     datasource_url = f'https://services.arcgis.com/{org_id}/ArcGIS/rest/services/{dataset_name}/FeatureServer/0?f=pjson&token={ago_token}'
+                    print(datasource_url)
                     response2 = requests.get(datasource_url)
                     #print(json.dumps(response2.json(), indent=2))
                     for i in response2.json()['fields']:
@@ -243,52 +280,50 @@ def main(itemid, expected_datasource_ids, new_datasource_itemid=None):
     # first run where we get datasource itemids and field names used in the dashboard json.
     find_and_modify_field_names(parsed_json)
 
-    print(f'found_datasource_itemids: {found_datasource_itemids}')
+    print(f'\nfound_datasource_itemids: {found_datasource_itemids}')
     # This is a safety measure in case we didn't account for all datasets informing a dashboard.
-    print('Asserting our expected datasource ids our as we expect them in the datasource.')
-    assert set(expected_datasource_ids) == found_datasource_itemids
+    print('Asserting our expected datasource ids are as we expect them in the datasource..')
+    assert set(expected_datasource_itemids) == found_datasource_itemids
     gather_datasource_field_names()
-    print(f'found_field_names: {found_field_names}')
+    print(f'\nfound_field_names: {found_field_names}')
 
     # Run a second time after our found_field_names set is fully populated
     find_and_modify_field_names(parsed_json)
 
 
-    f = open('./new_dashboard_json.txt', "w")
+    # Write locally after modification
+    f = open(f'./{target_dashboard_itemid}-MODIFIED-dashboard.json', "w")
     # Write with indent so it's readable while I'm spot-checking
     f.write(json.dumps(parsed_json, indent=2))
     f.close()
 
+    if dry_run:
+        print('\nDry run flag passed, not updating...')
+    else:
+        print('\nNow attempting to update the dashboard with our modified json..')
+        # Once we have our updated json, update the dashboard with it
+        regular_json = json.dumps(parsed_json)
+        #encoded_json = urllib.parse.quote(json.dumps(parsed_json))
+        #compressed_data = gzip.compress(json.dumps(parsed_json).encode('utf-8'))
 
-    # Once we have our updated json, update the dashboard with it
-    regular_json = json.dumps(parsed_json)
-    encoded_json = urllib.parse.quote(json.dumps(parsed_json))
-    compressed_data = gzip.compress(json.dumps(parsed_json).encode('utf-8'))
+        form_data = {
+            'f': 'json',
+            'id': target_dashboard_itemid,
+            'token': ago_token,
+            'text': regular_json
+        }
+        # Seems to be necessary to encode it all, otherwise we get an error about our URI being too long
+        # since the entire json contents are passed in the URL, encoding makes it more compact I believe?
+        encoded_form_data = urllib.parse.urlencode(form_data)
 
-    form_data = {
-        'f': 'json',
-        'id': itemid,
-        'token': ago_token,
-        'text': regular_json
-    }
-    encoded_form_data = urllib.parse.urlencode(form_data)
+        update_url = f'https://www.arcgis.com/sharing/rest/content/users/maps.phl.data/items/{target_dashboard_itemid}/update'
+        print(update_url)
 
-
-    update_url = f'https://www.arcgis.com/sharing/rest/content/users/maps.phl.data/items/{itemid}/update'
-    print(update_url)
-
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-
-    response = requests.post(update_url, headers=headers, data=encoded_form_data)
-    print(response.text)
-    assert response.status_code == 200
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = requests.post(update_url, headers=headers, data=encoded_form_data)
+        print(response.text)
+        assert response.status_code == 200
 
 
 if __name__ == '__main__':
-    itemid = 'some_dashboard_itemid'
-    #optional_new_datasource_itemid = 'optional_new_datasource_itemid'
-
-    #TODO: make this work
-    expected_datasource_ids = ['one_itemid', 'two_itemid']
-
-    main(itemid=itemid, expected_datasource_ids=expected_datasource_ids)
+    main()
